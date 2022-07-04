3b8c67528a0a2422b8928fa1bef3beb2229c890a
cookie <cookdon3@gmail.com>
2022-07-04 21:54:59 +0900

    update: 3-1章まで学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,29 +6,11 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function __invoke() {
-        return <<<EOF
-        <html>
-        <head>
-            <title>Hello</title>
-            <style>
-                body {
-                    font-size: 16px;
-                    color: #999;
-                }
-                h1 {
-                    font-size: 30pt;
-                    text-align: right;
-                    color: #eee;
-                    margin: -15px 0px 0px 0px;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Single Action</h1>
-            <p>これは、シングルアクションコントローラのアクションです。</p>
-        </body>
-        </html>
-        EOF;
+    public function index(Request $request) {
+        $data = [
+            'msg'=>'これはコントローラーから渡されたメッセージです。',
+            'id'=>$request->id
+        ];
+        return view('hello.index', $data);
     }
 }
