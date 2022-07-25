27b453ec362c5398c602a7c5404300d8a0916e66
cookie <cookdon3@gmail.com>
2022-07-25 22:05:37 +0900

    update: 3-2章まで学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    public function index(Request $request) {
+    public function index() {
         $data = [
-            'msg'=>'これはコントローラーから渡されたメッセージです。',
-            'id'=>$request->id
+            'msg' => 'お名前を入力してください。',
+        ];
+        return view('hello.index', $data);
+    }
+    public function post(Request $request) {
+        $msg = $request->msg;
+        $data = [
+            'msg' => 'こんにちは、' . $msg . 'さん！',
         ];
         return view('hello.index', $data);
     }
